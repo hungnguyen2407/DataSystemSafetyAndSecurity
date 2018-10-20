e137dc5970f20263ea3167703490e7bbb2aea7fb
hungnguyen2407 <14130047@st.hcmuaf.edu.vn>
2018-10-20 10:16:08 +0700

    update

--- src/SymmetricFileDES_CBC_PKCS5Padding.java
+++ src/SymmetricFileDES_CBC_PKCS5Padding.java
@@ -1,33 +1,34 @@
-
 import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateEncodingException;
-import java.util.Scanner;
 
-public class SymmetricFileDES {
+public class SymmetricFileDES_CBC_PKCS5Padding {
+
+    private static byte[] iv = new byte[8];
+    private static IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
     private static Key key;
+
     /**
      * Encrypt a plain text to cipher text
      *
-     * @param fileSrc : the key source
+     * @param fileSrc : the file source
      * @param fileDes : the file destination
      */
-    static void encrypt(String fileSrc, String fileDes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException {
+    static void encrypt(String fileSrc, String fileDes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
 
         KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
-        keyGenerator.init(56);
         key = keyGenerator.generateKey();
 
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec);
         FileInputStream fis = new FileInputStream(new File(fileSrc));
         FileOutputStream fos = new FileOutputStream(new File(fileDes));
 
@@ -53,10 +54,10 @@ public class SymmetricFileDES {
      * @param fileSrc : the file source
      * @param fileDes : the file destination
      */
-    static void decrypt(String fileSrc, String fileDes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException {
+    static void decrypt(String fileSrc, String fileDes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
 
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.DECRYPT_MODE, key);
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, ivParameterSpec);
 
         FileInputStream fis = new FileInputStream(new File(fileSrc));
         FileOutputStream fos = new FileOutputStream(new File(fileDes));
@@ -76,11 +77,13 @@ public class SymmetricFileDES {
         fos.close();
     }
 
-    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, CertificateEncodingException {
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, CertificateEncodingException, InvalidAlgorithmParameterException {
 
         System.out.println("Encrypt");
-        encrypt("", "");
+        encrypt("/Users/hungnguyen/Downloads/a.doc", "/Users/hungnguyen/Downloads/output.doc");
         System.out.println("Decrypt");
-        decrypt("", "");
+        decrypt("/Users/hungnguyen/Downloads/output.doc", "/Users/hungnguyen/Downloads/b.doc");
+
     }
+
 }
